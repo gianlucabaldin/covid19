e8b154d3ab7381692214bbbffd96e91443b13c56
Gianluca Baldin <gbaldin@intesys.it>
2020-04-16 10:37:51 +0200

    + NavButton spacing

--- src/component/NavButtons.jsx
+++ src/component/NavButtons.jsx
@@ -1,33 +1,35 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-    },
-  },
-}));
+import { Box } from '@material-ui/core';
 
 const NavButtons = () => {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
-      <Button variant="contained" color="primary">
-        Worldwide
-      </Button>
-      <Button variant="contained" color="primary">
-        Italy
-      </Button>
-      <Button variant="contained" color="primary" disabled>
-        Country List
-      </Button>
+    <Box
+      display="flex"
+      justifyContent="center"
+      m={1}
+      p={1}
+      bgcolor="background.paper"
+    >
+      <Box m={1}>
+        <Button variant="outlined" color="primary">
+          Worldwide
+        </Button>
+      </Box>
+      <Box m={1}>
+        <Button variant="outlined" color="primary">
+          Italy
+        </Button>
+      </Box>
+      <Box m={1}>
+        <Button variant="outlined" color="primary" disabled>
+          Country List
+        </Button>
+      </Box>
       {/* <Button variant="contained" color="primary" href="#contained-buttons">
         Link
       </Button> */}
-    </div>
+    </Box>
   );
 };
 
