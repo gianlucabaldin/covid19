d8d770f3ea017b47c1ebde053e9d6f4d0b3516a7
Gianluca Baldin <gbaldin@intesys.it>
2020-04-19 23:26:52 +0200

    new component ItalyRegions (WIP)

--- src/component/ItalyRegions.test.js
+++ src/component/ItalyRegions.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import ItalyRegions from './ItalyRegions';
 
-describe('ItalyChart', () => {
-  const wrapper = shallow(<ItalyChart />);
+describe('ItalyRegions', () => {
+  const wrapper = shallow(<ItalyRegions />);
 
   it('renders properly', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('contains a table', () => {
+    expect(
+      wrapper.find('WithStyles(ForwardRef(TableContainer))'),
+    ).toBeDefined();
   });
 });
--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,18 +1,20 @@
-import { mockResponseJson } from './mock/mockItalyHistoricalResponse';
+import { mockItalyHistoricalResponse } from './mock/mockItalyHistoricalResponse';
 
 export const API_ITALY_HYSTORICAL =
   'https://api.covid19api.com/total/dayone/country/italy';
+export const API_ITALY_REGION =
+  'https://openpuglia.org/api/?q=getdatapccovid-19';
 
 export const fetch = async (url = API_ITALY_HYSTORICAL, mock = true) => {
   if (mock) {
-    return mockResponseJson;
+    return mockItalyHistoricalResponse;
   }
   return fetch(url).then((res) => res.json());
 };
 
 export const fetchLastDay = async (mock = true) => {
   if (mock) {
-    return mockResponseJson[mockResponseJson.length - 1];
+    return mockItalyHistoricalResponse[mockItalyHistoricalResponse.length - 1];
   }
   return fetch().then((res) => res[res.length - 1]);
 };
