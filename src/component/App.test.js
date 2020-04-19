17e5586eb9011ef065c721a0b2b88a9afebbd53c
Gianluca Baldin <gbaldin@intesys.it>
2020-04-19 22:05:38 +0200

    fixed App.test gave act() error

--- src/component/App.test.js
+++ src/component/App.test.js
@@ -1,40 +1,24 @@
 import React from 'react';
 // import { render } from '@testing-library/react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import App from './App';
 import Header from './Header';
 import MainContainer from './MainContainer';
-/*
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-*/
 
 describe('App', () => {
-  let wrapper;
-  beforeAll(() => {
-    wrapper = shallow(<App />);
-  });
+  const wrapper = shallow(<App />);
 
   it('renders correctly', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
   describe('contains', () => {
-    beforeAll(() => {
-      wrapper = mount(<App />);
-    });
-
-    // Enzyme-matchers --> expect(wrapper).toContainReact(<ReactComponent />)
-    // https://github.com/FormidableLabs/enzyme-matchers/blob/master/packages/jest-enzyme/README.md#tocontainreact
     it('contains an Header component', () => {
-      expect(wrapper).toContainReact(<Header />);
+      expect(wrapper.find(<Header />)).toBeDefined();
     });
 
     it('contains MainContainer component', () => {
-      expect(wrapper).toContainReact(<MainContainer />);
+      expect(wrapper.find(<MainContainer />)).toBeDefined();
     });
   });
 });
