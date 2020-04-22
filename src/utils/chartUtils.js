dc7933e0e9dc90123193a215a65d0ee255259f9c
Gianluca Baldin <gbaldin@intesys.it>
2020-04-22 12:31:56 +0200

    chart: added last data value into shown values

--- src/utils/chartUtils.js
+++ src/utils/chartUtils.js
@@ -1,13 +1,17 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable import/prefer-default-export */
 // const DEFAUL_MAX_DATA_SIZE = 20;
 
 import { DEFAUL_MAX_DATA_SIZE } from './consts';
 
 // In order to have a "readable" chart, reduce if too much input values
 export const reduceData = (data, maxSize = DEFAUL_MAX_DATA_SIZE) => {
+  maxSize -= 1; // because the last array value is always added
   const { length } = data;
+  const lastVal = data[data.length - 1];
   if (length > maxSize) {
     const y = Math.round(length / maxSize);
     data = data.filter((el, i) => Math.round(i % y) === 0);
   }
-  return data;
+  return data.concat(lastVal);
 };
