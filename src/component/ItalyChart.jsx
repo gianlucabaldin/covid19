f4e6aa026d640b5c0a233315018f47a2f693ad3d
Gianluca Baldin <gbaldin@intesys.it>
2020-04-26 18:37:39 +0200

    respondive chart

--- src/component/ItalyChart.jsx
+++ src/component/ItalyChart.jsx
@@ -37,32 +37,32 @@ const ItalyChart = ({
         style={{ paddingLeft: '24', paddingRight: '24' }}
         spacing={2}
       >
-        <Grid item xs={4}>
+        <Grid item xs={12} md={6} lg={4}>
           <Chart
             series={[intensiveTherapy]}
             status={[STATUS.INTENSIVE_THERAPY]}
           />
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={12} md={6} lg={4}>
           <Chart
             style={{ width: 'inherit', padding: '24px' }}
             series={[totalHospitalized]}
             status={[STATUS.TOTAL_HOSPITALIZED]}
           />
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={12} md={6} lg={4}>
           <Chart series={[totalPositive]} status={[STATUS.TOTAL_POSITIVE]} />
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={12} md={6} lg={4}>
           <Chart
             series={[newDailyPositive]}
             status={[STATUS.NEW_DAILY_POSITIVE]}
           />
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={12} md={6} lg={4}>
           <Chart series={[dailyDeceased]} status={[STATUS.DAILY_DECEASED]} />
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={12} md={6} lg={4}>
           <Chart series={[dailySwabs]} status={[STATUS.DAILY_SWABS]} />
         </Grid>
       </Grid>
