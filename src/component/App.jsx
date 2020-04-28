2023eb1c50c26ba71872f67ab10992a9665efb2a
Gianluca Baldin <gbaldin@intesys.it>
2020-04-28 10:45:11 +0200

    + i18n-next translations

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import Header from './Header';
 import ItalyContainer from './ItalyContainer';
 
@@ -11,8 +12,14 @@ export const SECTIONS = {
 
 const App = () => {
   const [activeSection, setActiveSection] = useState(SECTIONS.ITALY);
+  const { t, i18n } = useTranslation();
   return (
     <div>
+      <h1>{t('welcome')}</h1>
+      <div>
+        <button onClick={() => i18n.changeLanguage('it')}>it</button>
+        <button onClick={() => i18n.changeLanguage('en')}>en</button>
+      </div>
       <Header />
       {activeSection === SECTIONS.ITALY && <ItalyContainer />}
     </div>
