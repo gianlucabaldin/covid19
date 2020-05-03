3f5bc25c6d31e32ecc692441c32e68e858e19f05
Gianluca Baldin <gbaldin@intesys.it>
2020-05-03 18:47:42 +0200

    refactoring and fixing cypress test

--- cypress/integration/WorldwideCountry.test.js
+++ cypress/integration/WorldwideCountry.test.js
@@ -1,17 +1,15 @@
 /* eslint-disable spaced-comment */
 /// <reference types="cypress" />
 
-context('CountryTable', () => {
-  beforeEach(() => {
+context('Worldwide container', () => {
+  before(() => {
     cy.visit('/');
     cy.get('[data-test-id="nav-button-worldwide"]').click();
   });
 
   context('should have', () => {
-    it('a container', () => {
-      cy.get('[data-test-id="worldwide-country-container"]').should(
-        'be.visible',
-      );
+    it('a table', () => {
+      cy.get('[data-test-id="worldwide-country-table"]').should('be.visible');
     });
     it('a table header', () => {
       cy.get('.MuiTableHead-root').should('be.visible');
@@ -20,17 +18,4 @@ context('CountryTable', () => {
       cy.get('.MuiTableBody-root').should('be.visible');
     });
   });
-
-  /* to review based on table shown columns (based on window width)
-  context('should show', () => {
-    it('2.859 total cases for Abruzzo Region', () => {
-      cy.get('[data-test-id="row-Abruzzo"]')
-        .find('td')
-        .then((cells) => {
-          const { length } = cells;
-          expect(cells[length - 1]).contains('33.820');
-        });
-    });
-  });
-  */
 });
